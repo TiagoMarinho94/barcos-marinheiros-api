28ae0327ecbaa837368def2f645aa6eb5c1b9462
Tiago Marinho <902508083@portic.ipp.pt>
2026-03-24 12:34:26 +0000

    Add marinheiros service - getMarinheirosByID

--- services/marinheirosService.js
+++ services/marinheirosService.js
@@ -2,6 +2,13 @@ const marinheirosRepository = require('../repositories/marinheirosRepository');
 
 exports.getAllMarinheiros = async function () {
     const result = await marinheirosRepository.getAllMarinheiros();
-    if (!result || result.length === 0) return null;
+    if (!result || result.length === 0)
+        return null;
+    return result;
+}
+exports.getMarinheirosByID = async function (id) {
+    const result = await marinheirosRepository.getMarinheirosByID(id);
+    if (!result)
+        return null;
     return result;
 }
