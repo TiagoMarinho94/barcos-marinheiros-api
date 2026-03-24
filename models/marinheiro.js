9f4fbccd296bdfabacb86164cf31fac92775296f
Tiago Marinho <902508083@portic.ipp.pt>
2026-03-24 14:48:16 +0000

    Add marinheiro model

---
+++ models/marinheiro.js
@@ -0,0 +1,8 @@
+class Marinheiro {
+    constructor (_nome, _classif, _idade) {
+        this.nome = _nome;
+        this.classificacao = _classif;
+        this.idade = _idade;
+    }
+}
+module.exports = Marinheiro;
