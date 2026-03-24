e174847b6ad0190da10c5dffa213b5fa584e4541
Tiago Marinho <902508083@portic.ipp.pt>
2026-03-24 12:28:32 +0000

    Add marinheiros repository - getMarinheirosByID

--- repositories/marinheirosRepository.js
+++ repositories/marinheirosRepository.js
@@ -4,9 +4,18 @@ exports.getAllMarinheiros = async function () {
     let lig;
     try {
         lig = await OracleDB.getConnection(dbConfig);
-        const result = await lig.execute('SELECT * FROM MARINHEIROS', [], { outFormat: OracleDB.OUT_FORMAT_OBJECT }
-        );
+        const result = await lig.execute('SELECT * FROM MARINHEIROS', [], { outFormat: OracleDB.OUT_FORMAT_OBJECT});
         return result.rows;
+    } finally {
+        if (lig) await lig.close();
+    }
+}
+exports.getMarinheirosByID = async function (id){
+    let lig;
+    try{
+        lig = await OracleDB.getConnection(dbConfig);
+        const result = await lig.execute('SELECT * FROM MARINHEIROS WHERE ID_MARINHEIRO = :1', [id], {outFormat: OracleDB.OUT_FORMAT_OBJECT});
+        return result.rows[0];
     } finally {
         if (lig) await lig.close();
     }
