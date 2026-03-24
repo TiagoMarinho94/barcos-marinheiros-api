2ce35d0c6c78c65e5f6eb0e559e890e5245900ec
Tiago Marinho <902508083@portic.ipp.pt>
2026-03-24 17:22:00 +0000

    Add barco route - createBarco

--- routes/barcosRoute.js
+++ routes/barcosRoute.js
@@ -5,4 +5,13 @@ const ctr = require('../controllers/barcosController');
 
 router.get('/', ctr.getAllBarcos);
 
+router.post('/',
+    celebrate({
+        body: Joi.object({
+            nome: Joi.string().min(2).required(),
+            cor: Joi.string().min(1).required()
+        })
+    }),
+ctr.createBarco);
+
 module.exports = router;
