1023942257128f19a8dc9f50c428a55268dfb6bc
Tiago Marinho <902508083@portic.ipp.pt>
2026-03-24 17:25:07 +0000

    Update barco repository - createBarco

--- repositories/barcosRepository.js
+++ repositories/barcosRepository.js
@@ -16,7 +16,7 @@ exports.createBarco = async function (_nome, _cor){
         lig = await OracleDB.getConnection(dbConfig);
         //procurar o maior ID existente
         const maxIdBarco = await lig.execute('SELECT NVL(MAX(ID_BARCO),0) + 1 AS NOVO_ID_BARCO FROM BARCOS', [], {outFormat: OracleDB.OUT_FORMAT_OBJECT});
-        const novoIdBarco = maxIdBarco.rows[0].NOVO_ID_Barco;
+        const novoIdBarco = maxIdBarco.rows[0].NOVO_ID_BARCO;
         //inserir agora os novos dados na tabela
         const result = await lig.execute('INSERT INTO BARCOS (ID_BARCO, NOME, COR) VALUES (:1, :2, :3)', [novoIdBarco,_nome, _cor], {outFormat: OracleDB.OUT_FORMAT_OBJECT});
         await lig.commit();
