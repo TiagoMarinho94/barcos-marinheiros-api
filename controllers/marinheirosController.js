baa72cbe68b4984e521db8bf229022a6ad178c56
Tiago Marinho <902508083@portic.ipp.pt>
2026-03-24 12:40:02 +0000

    Add marinheiros controller - getMarinheirosByID

--- controllers/marinheirosController.js
+++ controllers/marinheirosController.js
@@ -11,3 +11,14 @@ exports.getAllMarinheiros = async function (req, res) {
         res.status(500).json({ error: err.message });
     }
 }
+exports.getMarinheirosByID = async function (req,res) {
+    try {
+        var result = await MarinheirosSrv.getMarinheirosByID(req.params.id);
+        if(!result)
+            return res.status(404).json({ error: 'Não foram encontrados marinheiros com esse ID' });
+        res.json(result);
+    }
+    catch (err){
+        res.status(500).json({error: err.message});
+    }
+}
