283a74a1ae1b4e059844414e172b3126322e1aef
Tiago Marinho <902508083@portic.ipp.pt>
2026-03-26 23:36:46 +0000

    Update reservas service - getReservasByIDMarinheiro

--- services/reservasService.js
+++ services/reservasService.js
@@ -1,8 +1,9 @@
 const reservasRepository = require('../repositories/reservasRepository');
+const ReservasDTO = require('../models/reservas_dto');
 
 exports.getReservasByIDMarinheiro = async function (id) {
     const result = await reservasRepository.getReservasByIDMarinheiro(id);
     if (!result || result.length === 0)
         return null;
-    return result;
+    return result.map(item => ReservasDTO.toDetail(item));
 }
