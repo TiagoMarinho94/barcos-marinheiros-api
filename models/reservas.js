342c88a51a5b88737a8ceba68a306c8d42b318b4
Tiago Marinho <902508083@portic.ipp.pt>
2026-03-26 22:29:07 +0000

    Add Reserva model

---
+++ models/reservas.js
@@ -0,0 +1,8 @@
+class Reserva {
+    constructor(_id_marinheiro, _id_barco, _data) {
+        this.id_marinheiro = _id_marinheiro;
+        this.id_barco = _id_barco;
+        this.data = _data;
+    }
+}
+module.exports = Reserva;
