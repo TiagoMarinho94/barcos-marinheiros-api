7b8a2f6c06718412a5e06bbc1496255e52168cf2
Tiago Marinho <902508083@portic.ipp.pt>
2026-03-29 23:13:21 +0100

    Add marinheiros controller - deleteMarinheiro

--- controllers/marinheirosController.js
+++ controllers/marinheirosController.js
@@ -55,3 +55,17 @@ exports.updateMarinheirosByID = async function (req,res) {
         res.status(500).json({error: err.message});
     }
 }
+exports.deleteMarinheiro = async function (req,res) {
+    try {
+        var result = await MarinheirosSrv.deleteMarinheiro(req.params.id);
+        //se marinheiro tem reservas
+        if(result === -1)
+            return res.status(409).json({ error: 'Marinheiro tem reservas, não é possível apagar'});
+        if(!result)
+            return res.status(404).json({ error: 'Marinheiro não encontrado' });
+        res.sendStatus(204);
+    }
+    catch (err){
+        res.status(500).json({error: err.message});
+    }
+}
