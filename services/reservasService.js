65efc6aea5fc162365f34f5798d2baa9e1fb0b79
Tiago Marinho <902508083@portic.ipp.pt>
2026-03-29 18:47:46 +0100

    Add reservas service - createReserva

--- services/reservasService.js
+++ services/reservasService.js
@@ -1,4 +1,6 @@
 const reservasRepository = require('../repositories/reservasRepository');
+const marinheirosRepository = require('../repositories/marinheirosRepository');
+const barcosRepository = require('../repositories/barcosRepository');
 const ReservasDTO = require('../models/reservas_dto');
 
 exports.getReservasByIDMarinheiro = async function (id) {
@@ -6,4 +8,18 @@ exports.getReservasByIDMarinheiro = async function (id) {
     if (!result || result.length === 0)
         return null;
     return result.map(item => ReservasDTO.toDetail(item));
+}
+exports.createReserva = async function (_idmarinheiro, _idbarco, _data) {
+    //procurar se marinheiro existe com função que já existe
+    const mar = await marinheirosRepository.getMarinheirosByID(_idmarinheiro)
+    if (!mar) return -1;
+    //procurar se barco existe com função que já existe
+    const bar = await barcosRepository.getBarcosByID(_idbarco)
+    if (!bar) return -2;
+    //Se ambos existirem posso criar a reserva
+    //fiz isto pois como alterar a base de dados não faz parte deste trabalho, assim garante que a logica do negocio
+    const result = await reservasRepository.createReserva(_idmarinheiro, _idbarco, _data);
+    if (!result || result === 0)
+        return null;
+    return result;
 }
