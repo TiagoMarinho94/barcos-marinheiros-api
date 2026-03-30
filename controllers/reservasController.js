f3e9b8b68d1ab91ebe9e2ef41ba78e07799c9725
Tiago Marinho <902508083@portic.ipp.pt>
2026-03-30 21:41:34 +0100

    Add reservas controller - GET AllReservas-extra

--- controllers/reservasController.js
+++ controllers/reservasController.js
@@ -48,3 +48,14 @@ exports.deleteReserva = async function (req,res) {
         res.status(500).json({error: err.message});
     }
 }
+exports.getAllReservas = async function (req, res) {
+    try {
+        var result = await ReservasSrv.getAllReservas();
+        if (!result)
+            return res.status(404).json({ error: 'Não existem reservas' });
+        res.json(result);
+    }
+    catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+}
