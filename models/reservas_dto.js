d79be722c35f9d1223c5f382f8b5cb57d1ec4c42
Tiago Marinho <902508083@portic.ipp.pt>
2026-04-11 13:01:22 +0100

    Add nome_marinheiro to reservas DTO

--- models/reservas_dto.js
+++ models/reservas_dto.js
@@ -1,6 +1,7 @@
 exports.toDetail = function (obj){
     return {
         'id_marinheiro': obj.ID_MARINHEIRO,
+        'nome_marinheiro': obj.NOME_MARINHEIRO,
         'id_barco': obj.ID_BARCO,
         'nome_barco': obj.NOME_BARCO,
         'data': obj.DATA.toISOString().split('T')[0],
